1f2a806286605f847602cd90a12d61b2f26b7174
José Neto <73612432+jose-raposo@users.noreply.github.com>
2021-01-06 18:45:58 -0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"jraposoneto60@gmail.com\"
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
